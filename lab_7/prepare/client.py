204dafea65eb0e5707b94c3403bffd48d3e54672
Gniloe-Aloe <garaino@mail.ru>
2022-05-14 20:10:34 +0300

    delete

--- lab_7/prepare/client.py
+++
@@ -1,9 +0,0 @@
-# вариант 10
-import socket
-
-client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_sock.connect(('127.0.0.1', 53210))
-client_sock.sendall(b'Hello string from Kosinov Denis!')
-data = client_sock.recv(1024)
-client_sock.close()
-print('Received', repr(data))
